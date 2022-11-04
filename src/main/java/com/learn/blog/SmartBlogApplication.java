38e57c613bdbc6017feb8a8154d2195ac1e32255
wenqi <qik1992@163.com>
2022-11-04 21:27:42 +0800

    整合通用mapper

--- src/main/java/com/learn/blog/SmartBlogApplication.java
+++ src/main/java/com/learn/blog/SmartBlogApplication.java
@@ -1,11 +1,11 @@
 package com.learn.blog;
 
 import com.learn.blog.config.DynamicDataSourceConfig;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Import;
+import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @Import({DynamicDataSourceConfig.class})
